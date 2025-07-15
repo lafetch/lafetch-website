1d7e2e28caba1e4773ce36617a0c8e8ada8b7ee9
LaFetch <admin@la-fetch.com>
2025-07-15 15:09:29 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import AppPlugin from "@/components/landingPage/AppPlugin";
 import BottomVideoSection from "@/components/landingPage/BottomVideoSection";
 import BrandsList from "@/components/landingPage/BrandsList";
@@ -6,13 +7,8 @@ import HeroSection from "@/components/landingPage/HeroSection";
 import Mission from "@/components/landingPage/Mission";
 import VideoSection from "@/components/landingPage/VideoSection";
 import MobilePage from "@/components/MobilePage";
-import type { Metadata } from "next";
 
-
-
-
-
-
+{
 
 export const metadata: Metadata = {
   title: "Lafetch",
